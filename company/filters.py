21e4a2617a03fc2b847f7abb24b532323a4c1d17
unknown <rubycpoovelil7@gmail.com>
2020-05-11 11:46:40 +0530

    remove

--- company/filters.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-import django_filters
-from .models import com_pro
-
-class UserFilter(django_filters.FilterSet):
-    class Meta:
-        model = com_pro
-        fields = ['com_username', 'com_desc', 'com_place', 'com_pincode', 'com_dt', 'com_state', 'com_email' ,'com_mob' , 'com_country', ]
-        
--- company/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms  
-from company.models import com_pro  
-class CompanyForm(forms.ModelForm):  
-    class Meta:  
-        model = com_pro  
-        fields = "__all__"  
